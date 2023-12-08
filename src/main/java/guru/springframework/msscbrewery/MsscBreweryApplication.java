78841ac8e091d9a80dd5231e8a775afa599c84f6
Ukasha <onticxt@gmail.com>
2023-12-08 11:31:15 -0500

    second commit

--- src/main/java/guru/springframework/msscbrewery/MsscBreweryApplication.java
+++ src/main/java/guru/springframework/msscbrewery/MsscBreweryApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MsscBreweryApplication {
 
+	// my comments
 	public static void main(String[] args) {
 		SpringApplication.run(MsscBreweryApplication.class, args);
 	}
