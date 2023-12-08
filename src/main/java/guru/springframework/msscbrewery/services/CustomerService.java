618358eb4cad7aeb66b3c0f062ffaad449a9d547
Ukasha <onticxt@gmail.com>
2023-12-08 11:24:33 -0500

    first commit

---
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerService.java
@@ -0,0 +1,11 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.UUID;
+
+public interface CustomerService {
+    CustomerDto getCustomerById(UUID customerId);
+}
