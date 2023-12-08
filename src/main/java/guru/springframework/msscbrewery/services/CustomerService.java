b7b731599b959c7e8052fd3af2267bba731b72ef
Ukasha <onticxt@gmail.com>
2023-12-08 11:51:23 -0500

    Added customer crud

--- src/main/java/guru/springframework/msscbrewery/services/CustomerService.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerService.java
@@ -1,5 +1,6 @@
 package guru.springframework.msscbrewery.services;
 
+import guru.springframework.msscbrewery.web.model.BeerDto;
 import guru.springframework.msscbrewery.web.model.CustomerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,4 +9,7 @@ import java.util.UUID;
 
 public interface CustomerService {
     CustomerDto getCustomerById(UUID customerId);
+    CustomerDto saveNewCustomer(CustomerDto customerDto);
+    void updateCustomer(UUID customerId, CustomerDto customerDto);
+    void deleteById(UUID customerId);
 }
--- src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -1,5 +1,6 @@
 package guru.springframework.msscbrewery.services;
 
+import guru.springframework.msscbrewery.web.model.BeerDto;
 import guru.springframework.msscbrewery.web.model.CustomerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,5 +12,18 @@ public class CustomerServiceImpl implements CustomerService{
     @Override
     public CustomerDto getCustomerById(UUID customerId){
         return CustomerDto.builder().id(UUID.randomUUID()).name("Joe Buck").build();
+    }
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto){
+        return customerDto.builder().id(UUID.randomUUID()).build();
+    }
+    @Override
+    public void updateCustomer(UUID customerId,CustomerDto customerDto){
+
+    }
+
+    @Override
+    public void deleteById(UUID beerId){
+
     }
 }
